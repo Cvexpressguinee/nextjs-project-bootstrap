b77d9ecf1650fcad783186986ac19646bd4461d7
Cvexpressguinee <bd3477628@gmail.com>
2025-07-05 02:55:03 +0000

    Add .next/static/chunks/src_app_page_tsx_a993527f._.js 1751684103056

---
+++ .next/static/chunks/src_app_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_9b242079._.js",
+  "static/chunks/src_3be483d5._.js"
+],
+    source: "dynamic"
+});
